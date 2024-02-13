36a7acbde0e21e955c2b10f0a6529d17e7317701
ORI-Muchim <whals467746@gmail.com>
2024-02-14 01:23:34 +0900

    Edit inference.py

--- inference.py
+++ inference.py
@@ -3,16 +3,20 @@ import argparse
 import subprocess
 
 def main():
-    if len(sys.argv) != 4:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_name', type=str, help='Name of the model')
+    parser.add_argument('model_step', type=str, help='Step of the model')
+    parser.add_argument('--poly', action='store_true', help='Options for Poly-Language')
+
+    args, unknown = parser.parse_known_args()
+
+    if unknown:
+        print(f"Unknown arguments: {unknown}")
         print("Usage: python main.py {model_name} {model_step} [--poly](Optional)")
         sys.exit(1)
 
-    model_name = sys.argv[1]
-    model_step = sys.argv[2]
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--poly', action='store_true', help='Options for Poly-Language')
-    args = parser.parse_args(sys.argv[3:])
+    model_name = args.model_name
+    model_step = args.model_step
 
     if args.poly:
         command = ["python", "./vits2/inference.py", model_name, model_step, "--poly"]
